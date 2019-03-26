f0f608ac127ebe6b8fa0e31d8bcb1e82b60f64f4
Kelsey Low <kklowlow@gmail.com>
2019-03-26 03:35:49 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -38,6 +38,8 @@ end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
 "SELECT projects.category, pledges.amount FROM projects
+  INNER JOIN pledges
+    ON projects.id = pledges.project_id
   
 "
 end
